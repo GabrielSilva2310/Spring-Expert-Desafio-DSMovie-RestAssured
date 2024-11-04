fa6d2a02607063e59f509487afd7fe64c925a008
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-11-04 01:51:11 -0300

    Created Project

---
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsmovie.controllers;
+
+
+import org.junit.jupiter.api.Test;
+
+public class MovieControllerRA {
+	
+	@Test
+	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {	
+	}
+	
+	@Test
+	public void findByIdShouldReturnMovieWhenIdExists() {		
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	}
+	
+	@Test
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() {		
+	}
+	
+	@Test
+	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+	}
+	
+	@Test
+	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+	}
+}
